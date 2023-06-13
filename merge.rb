b6d981513f261b6dfbe8a03f9875f7046192054c
Ben W. Brumfield <benwbrum@gmail.com>
2023-06-13 10:23:05 -0500

    Pre-refactoring commit reflecting work since December 2022

---
+++ merge.rb
@@ -0,0 +1,342 @@
+#!/usr/bin/env ruby
+require 'nokogiri'
+require 'text'
+require 'pry-byebug'
+
+# TODO check for outliers via y-axis in coordinates instead of ordering
+# TODO change to span-specific checks
+# TODO change to passes by descending character order
+
+
+# TODO consider pruning punctuation to get semi-fuzzy matches
+
+
+def unique_words_in_array(array)
+  array.tally.select{|k,v| v==1}.keys  
+end
+
+def remove_outliers(alignment_map)
+  # TODO -- make this more sophisticated; clean up
+  # figure out standard deviation
+  raw_ids = alignment_map.values.map{|e| e['ID']}
+  ids = raw_ids.map{|id| id.sub('S','').to_i }
+  sorted_ids = ids.sort
+  differences = []
+  removal_count = 0
+  ids.each_with_index do |id,i|
+    if i > 0
+      difference = id - ids[i-1]
+      differences << difference
+      if difference.negative?
+        # this element went down, so is probably an abberration
+        alignment_map.delete_if {|k,v| v['ID'] == "S#{id}"}
+        removal_count += 1
+      end
+
+    end
+  end
+  
+
+  # walk through list, removing outliers
+  # remove remaining out-of-order elements
+  print "remove_outliers removed #{removal_count} out-of-order elements\n"
+end
+
+
+# Print usage information and exit if the -h option is present
+if ARGV.include?('-h')
+  puts "Usage: merge.rb CORRECTED_FILE ALTO_FILE"
+  exit
+end
+
+# Parse command line arguments
+corrected_file = ARGV[0]
+alto_file = ARGV[1]
+
+# Parse the ALTO-XML file using Nokogiri
+alto_doc = Nokogiri::XML(File.read(alto_file))
+
+# create a hash of words we can use
+alto_words = []
+alto_doc.xpath('//*[@CONTENT]').each_with_index do |node,i|
+  alto_words[i] = {string: node['CONTENT'], element: node}
+end
+
+# Read the corrected text from the plaintext file
+corrected_text = File.read(corrected_file)
+
+# 
+corrected_words = corrected_text.split
+possible_anchors = unique_words_in_array(corrected_words)
+
+# map the index of each word to a corresponding node in the text
+alignment_map = {}
+print "Phase A: Aligning words based on exact matches\n"
+
+previous_match = nil
+possible_anchors.each do |candidate|
+  matching_nodes = alto_words.select {|item| item[:string] == candidate  }
+
+  if matching_nodes.count == 1
+    match = matching_nodes.first[:element]
+    # one unique match means we've found an anchor, but we need to check for order within the document
+    if true || !previous_match || previous_match['ID'] < match['ID']
+      index_of_word = corrected_words.index(candidate)
+      alignment_map[index_of_word] = match
+      previous_match = match
+    end
+  end
+end
+print "Pass 1 anchor count: #{alignment_map.size}\t(#{(100 * alignment_map.size.to_f/corrected_words.size.to_f).floor}% aligned)\n"
+#ordered_ids= alignment_map.sort.map{|a| a[1]['ID']}.join("\n")
+#print "Pass 1 ordered IDs:\n#{ordered_ids}\n\n"
+
+remove_outliers(alignment_map)
+# what's going on here?
+#    [1] 129,
+#    [2] 135,
+#    [3] 124,
+#    [4] 155
+
+
+
+
+# now we have top-level segmentation matching unique words within the page.
+# let's do the same for words within each range
+
+# TODO refactor to avoid code duplication
+2.upto(6).each do |pass_number| 
+  previous_index = nil
+  alignment_map.keys.sort.each_with_index do |key,i|
+    if i==0
+      previous_index=key
+    else
+      current_index = key
+      # get the range between the two
+      start_range = previous_index+1
+      end_range = current_index-1
+      
+      corrected_range = corrected_words[start_range..end_range]
+      unique_words = unique_words_in_array(corrected_range)
+      # get the range of alto_words that corresponds to the current range (segments within the anchors bounding the current range)
+
+      alto_start = alto_words.map{|e| e[:element]}.index(alignment_map[previous_index])
+      alto_end = alto_words.map{|e| e[:element]}.index(alignment_map[current_index])
+      alto_range = alto_words[alto_start..alto_end]
+
+      # walk through each word, finding the index of the word within corrected_words (start_range+i)
+      unique_words.each do |candidate|
+        # look for the word in the alto_range
+        alto_range_index = alto_range.index {|element| element[:string] == candidate}
+        if alto_range_index
+          # associate words that are found if they do not violate word order
+          corrected_index = corrected_range.index(candidate)+start_range
+          alto_words_index = alto_range_index + alto_start
+          alignment_map[corrected_index] = alto_range[alto_range_index][:element]
+        end
+      end
+      previous_index=key
+    end
+  end
+  remove_outliers(alignment_map)
+  print "Pass #{pass_number} anchor count: #{alignment_map.size}\t(#{100 * alignment_map.size.to_f/corrected_words.size.to_f}% aligned)\n"
+  ordered_ids= alignment_map.sort.map{|a| a[1]['ID']}.join("\n")
+#  print "Pass #{pass_number} ordered IDs:\n#{ordered_ids}\n\n"
+end
+
+print "Span lengths to resolve\n"
+old_key=nil
+alignment_map.keys.sort.each_with_index do |key,i|
+  if i>0 && key-old_key > 1
+    print "#{i}\t#{key-old_key - 1}\t#\n"
+  end
+  old_key = key
+end
+
+
+print "Phase B: Aligning words based on fuzzy matching"
+previous_index = nil
+alignment_map.keys.sort.each_with_index do |key,i|
+  if i==0
+    previous_index=key
+  else
+    current_index = key
+    # get the range between the two
+    start_range = previous_index+1
+    end_range = current_index-1
+    
+    corrected_range = corrected_words[start_range..end_range]
+    unique_words = unique_words_in_array(corrected_range)
+    # get the range of alto_words that corresponds to the current range (segments within the anchors bounding the current range)
+
+    alto_start = alto_words.map{|e| e[:element]}.index(alignment_map[previous_index])
+    alto_end = alto_words.map{|e| e[:element]}.index(alignment_map[current_index])
+    alto_range = alto_words[alto_start..alto_end]
+
+    # walk through each word longer than three characters looking for close matches
+    unique_words.select{|w| w.length>=3}.each do |candidate|
+      long_words = alto_range.select{|w| w[:string].length >=3}
+      fuzzy_match_array = long_words.map{|w| [w[:string], Text::Levenshtein.distance(candidate, w[:string]).to_f/candidate.length]}
+      sorted_fuzzy_matches = fuzzy_match_array.sort{|a,b| a[1]<=>b[1]}
+      best_match = sorted_fuzzy_matches.first
+      if best_match[1] < 0.45
+        # print "#{best_match[1].round(2)}\t#{candidate}\t#{best_match[0]}\n" if best_match[1] < 0.45
+        alto_range_index = alto_range.index {|element| element[:string] == best_match[0]}
+        corrected_index = corrected_range.index(candidate)+start_range
+        alto_words_index = alto_range_index + alto_start
+        alignment_map[corrected_index] = alto_range[alto_range_index][:element]
+      end
+    end
+    previous_index=key
+  end
+end
+remove_outliers(alignment_map)
+print "Phase B anchor count: #{alignment_map.size}\t(#{100 * alignment_map.size.to_f/corrected_words.size.to_f}% aligned)\n"
+ordered_ids= alignment_map.sort.map{|a| a[1]['ID']}.join("\n")
+
+print "Span lengths to resolve\n"
+old_key=nil
+alignment_map.keys.sort.each_with_index do |key,i|
+  if i>0 && key-old_key > 1
+    print "#{i}\t#{key-old_key - 1}\t#\n"
+  end
+  old_key = key
+end
+
+print "Phase C: Aligning words based on word counts\n"
+previous_index = nil
+alignment_map.keys.sort.each_with_index do |key,i|
+  if i==0
+    previous_index=key
+  else
+    current_index = key
+    if current_index - previous_index == 1
+      # null case -- no gap in mapping
+    elsif current_index - previous_index == 2
+      # simple case -- only one word has not been mapped
+      missing_index = current_index - 1
+      missing_alto = alto_words.map{|e| e[:element]}.index(alignment_map[previous_index])+1
+      alignment_map[missing_index] = alto_words[missing_alto][:element]
+    else
+      # get the range between the two
+      start_range = previous_index+1
+      end_range = current_index-1
+      if end_range - start_range > 0
+        # TODO refactor range calculation
+        corrected_range = corrected_words[start_range..end_range]
+        alto_start = alto_words.map{|e| e[:element]}.index(alignment_map[previous_index])+1
+        alto_end = alto_words.map{|e| e[:element]}.index(alignment_map[current_index])-1
+
+        alto_range = alto_words[alto_start..alto_end]
+
+        print("#{corrected_range.count}\tA\t#{alto_range.map{|e| e[:string]}.join(' ')}\n\tC\t#{corrected_range.join(' ')}\n\n")
+        if !alto_range.map{|e| e[:string]}.join('').match? /\S/
+          print("WARNING: Misalignment at index #{current_index}.  Corrected text: #{corrected_range.join(' ')}\n")
+        end
+        if alto_range.count == corrected_range.count
+          corrected_range.each_with_index do |candidate, range_index|
+            corrected_index = range_index+start_range
+            alignment_map[corrected_index] = alto_range[range_index][:element]
+          end
+        else
+          print("Unequal alignment #{corrected_range.count}::#{alto_range.count}:\n#{corrected_range.join(' ')}\ninto\n#{alto_range.map{|e| e[:string]}.join(' ')}\n\n")
+          # match each element with the corresponding one, then consolidate the last elements
+          if alto_range.size==0
+            # TODO: find previous item and append it
+            print "WARNING: no range for #{corrected_range.join(' ')}\n"
+          else
+            corrected_range.each_with_index do |candidate, range_index|
+              corrected_index = range_index+start_range
+              if range_index <= alto_range.size
+                # this is the last of the ALTO elements; consolidate all remaining corrected words into the last element
+              else
+                # if this is the last of the corrected words, leave remaining alto elements unmapped.  Regardless, map the corresponding index
+                alignment_map[corrected_index] = alto_range[range_index][:element]
+              end
+            end
+          end          
+        end
+      end
+    end    
+    previous_index=key 
+  end
+end
+
+# don't forget initial and final spans
+first_aligned_index = alignment_map.keys.min
+if first_aligned_index > 0
+  # how many alto words precede the first aligned one?
+  first_aligned_alto_element = alignment_map[first_aligned_index]
+  index_in_alto = alto_words.map{|e| e[:element]}.index(alignment_map[first_aligned_index])
+  if index_in_alto == first_aligned_index
+    0.upto(index_in_alto-1) do |i|
+      alignment_map[i]=alto_words[i][:element]
+    end
+  else
+    print "WARNING: unequal number of leading elements in alto vs. corrected text\n"
+  end 
+end
+
+
+last_aligned_index = alignment_map.keys.max
+if last_aligned_index < corrected_words.size-1
+  # how many alto words follow the last aligned one?
+  last_aligned_alto_element = alignment_map[last_aligned_index]
+
+  # TODO: I'm too foggy for this right now
+end
+
+
+print "Alignment count after alignment by word count: #{alignment_map.size}\t(#{100 * alignment_map.size.to_f/corrected_words.size.to_f}% aligned)\n"
+
+print "Phase D: Merging aligned words into ALTO-XML\n"
+unaligned_corrected=[]
+corrected_words.each_with_index do |corrected,i|
+  if alignment_map[i]
+    alignment_map[i]['CONTENT'] = corrected
+  else
+    unaligned_corrected << [i,corrected]
+  end
+end
+
+print "Phase E: Merging unaligned words into ALTO-XML\n"
+
+
+print "Phase F: Remove unaligned XML elements"
+aligned_elements = alignment_map.values
+alto_words.each do |e|
+  unless aligned_elements.include?(e[:element])
+    alto_doc.delete(e[:element])
+  end
+end
+
+binding.pry 
+# Save the updated ALTO-XML file
+#File.write(alto_file, alto_doc.to_xml)
+
+
+# Known Issues
+
+# Current problem:  mis-alignment that works forward, rather than backward, as in 202 (corrected) mapped to 206 (ALTO)
+# This leaves significant gaps
+# 199.upto(214) {|i| print "#{i}\t#{corrected_words[i+1]}\t#{alto_words[i][:string]}\n"}
+BAD_ALGINMENT_DATA =<<EOF
+199	appear	appear
+200	to	to
+201	occupy	occussy
+202	a	as
+203	space	face
+204	of	of
+205	about	about
+206	4	a
+207	miles	mile
+208	in	as
+209	a	as
+210	NE	Not'
+211	direction	direction.
+212	From	From
+213	Point	Point
+214	Danger	danger
+EOF
+
+
